9cfa9b0bdf6669f87efdfd5a41762855b571ed19
mat-onet <33892257+mat-onet@users.noreply.github.com>
2026-05-21 13:12:58 -0500

    fix: change all ravenburst.app references to ravenburst.com

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import tailwind from '@astrojs/tailwind'
 
 export default defineConfig({
   integrations: [react(), tailwind()],
-  site: 'https://ravenburst.app',
+  site: 'https://ravenburst.com',
 })
